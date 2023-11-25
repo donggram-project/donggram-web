0a3cd89a571c3d321480f54f69c70379a3c1017f
TaeHo <otr0811@gmail.com>
2023-11-25 15:05:26 +0900

    memberInfo_put

--- src/components/MainInfoFolder/MainInfo.tsx
+++ src/components/MainInfoFolder/MainInfo.tsx
@@ -14,86 +14,142 @@ import {
   SaveButton,
   ButtonText,
   TextSelectBox,
-  TextNImageBox,
-  CloseXimage,
-  Texting,
+  ImageUpLoad,
+  PlaceholderImage,
+  DeleteButton,
 } from "./MainInfoStyle";
+import { useCallback } from "react";
+import { useRouter } from "next/router";
+
+interface DataRow {
+  studentId: string;
+  memberName: string;
+  major1: string;
+}
 
 export function MainInfo() {
+  const router = useRouter();
   const [userData, setUserData] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState("");
+  const [userCollege, setUserCollege] = useState("");
+  const [imageSrc, setImageSrc]: any = useState(null);
+  // 출처: https://itprogramming119.tistory.com/entry/React-이미지-파일업로드-미리보기-구현하기 [코딩병원:티스토리]
+
+  const onSubmit = useCallback(() => {
+    console.log("이름 = ", userName);
+    console.log("학번 = ", userId);
+    console.log("전공 =", userCollege);
+    console.log("사진 = ", imageSrc);
+    customAxios
+      .put(`http://13.125.162.181:8084/member/`, {
+        memberName: userName,
+        studentId: userId,
+        major1: userCollege,
+        profileImage: imageSrc,
+      })
+      .then(() => {
+        console.log("put success");
+        router.reload();
+      })
+      .catch((error) => console.error("에러 발생 이유: ", error.response));
+  }, [userName, userId, userCollege, imageSrc]);
+
+  useEffect(() => {
+    console.log("이름 = ", userName);
+    console.log("학번 = ", userId);
+    console.log("전공 =", userCollege);
+    console.log("사진 = ", imageSrc);
+  }),
+    [userName, userId, userCollege, imageSrc];
+
   useEffect(() => {
     customAxios
-      .get(`http://13.125.162.181:8084/member`) //본인 학번에 맞는 정보 불러오도록 수정
+      .get(`http://13.125.162.181:8084/member`)
       .then((res) => {
-        setUserData(res.data.data),
-          // handleClicked(response.data.data[0].clubId)]
-          // console.log(res.data.data.studentId)
-          console.log(userData);
+        setUserData(res.data.data);
+        setUserName(res.data.data.memberName);
+        setUserId(res.data.data.studentId);
+        setUserCollege(res.data.data.major1);
+        setImageSrc(res.data.data.profileImage);
+
+        console.log(res.data.data);
+        console.log(userData);
         console.log("불러오기 성공");
       })
       .catch((error) => console.error("에러:에러!!!!!!!!!!!!!!", error));
   }, [setUserData]);
+
+  const onUpload = (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result || null); // 파일의 컨텐츠
+        resolve();
+      };
+    });
+  };
+  //itprogramming119.tistory.com/entry/React-이미지-파일업로드-미리보기-구현하기 [코딩병원:티스토리]
+  const resetImageSrc = useCallback(() => {
+    setImageSrc(null);
+  }, [setImageSrc]);
+
   return (
     <PageContainer>
       <UserInfoText>회원정보</UserInfoText>
       <TextNImageContainer>
         <OtherInfoTextContainer>
           <OtherInfoText>이름</OtherInfoText>
-          <TextSelectBox>
-            <Texting id="NoneBox">{userData.memberName}</Texting>
-          </TextSelectBox>
+          <TextSelectBox
+            id="name"
+            placeholder={userData.memberName}
+            onChange={(event) => setUserName(event.target.value)}
+            value={userName}
+          ></TextSelectBox>
 
           <OtherInfoText>학번</OtherInfoText>
-          <TextSelectBox>
-            <Texting id="NoneBox">{userData.studentId}</Texting>
-          </TextSelectBox>
+          <TextSelectBox
+            id="id"
+            placeholder={userData.studentId}
+            onChange={(event) => setUserId(event.target.value)}
+            value={userId}
+          ></TextSelectBox>
 
           <OtherInfoText>소속</OtherInfoText>
-          <TextSelectBox>
-            <TextNImageBox>
-              <Texting
-                style={{
-                  color:
-                    userData.college1 && userData.major1
-                      ? "var(--cool-gray-90, #21272a)"
-                      : "transparent",
-                }}
-              >
-                {userData.college1 &&
-                  userData.major1 &&
-                  `${userData.college1}-${userData.major1}`}
-              </Texting>
-              {userData.college1 && userData.major1 && (
-                <CloseXimage src="/close.svg" alt="X" />
-              )}
-
-              <Texting
-                style={{
-                  color:
-                    userData.college2 && userData.major2
-                      ? "var(--cool-gray-90, #21272a)"
-                      : "transparent",
-                }}
-              >
-                {userData.college2 &&
-                  userData.major2 &&
-                  `${userData.college2}-${userData.major2}`}
-              </Texting>
-              {userData.college2 && userData.major2 && (
-                <CloseXimage src="/close.svg" alt="X" />
-              )}
-              {/* API로 소속 여러개 */}
-            </TextNImageBox>
-          </TextSelectBox>
+          <TextSelectBox
+            id="college"
+            placeholder={userData.major1}
+            onChange={(event) => setUserCollege(event.target.value)}
+            value={userCollege}
+          ></TextSelectBox>
           <OtherInfoText>내 동아리</OtherInfoText>
         </OtherInfoTextContainer>
-        <ClubImageCss src={clubimage} alt="club main image" />
+
+        {imageSrc ? (
+          <ClubImageCss src={imageSrc} />
+        ) : (
+          <PlaceholderImage src={clubimage} alt="Placeholder" />
+        )}
       </TextNImageContainer>
-      <ClubTable />
+      <ImageUpLoad>
+        <input
+          accept="image/*"
+          multiple
+          type="file"
+          onChange={(e) => onUpload(e)}
+        />
+        <DeleteButton onClick={resetImageSrc}>삭제</DeleteButton>
+      </ImageUpLoad>
+
+      <ClubTable clubList={userData.clubList} />
 
       <BottomBorder />
       <SaveButton>
-        <ButtonText>저장</ButtonText>
+        <ButtonText onClick={onSubmit}>저장</ButtonText>{" "}
+        {/*추후에 멤버 정보 변경 기능 추가 필요 */}
       </SaveButton>
     </PageContainer>
   );
